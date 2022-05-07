fa670ab93a0f2376759a2a71d77403d64c8c84f7
Teodor Osmanliev <TPOsmanliev19@codingburgas.bg>
2022-05-07 14:51:21 +0300

    Add function for printing genetics theme

--- BiologyProject/BiologyData/Functions.h
+++ BiologyProject/BiologyData/Functions.h
@@ -1,5 +1,20 @@
 #pragma once
+#include <iostream>
 
 using namespace std;
 
 
+void printGeneticsTheme()
+{
+	cout <<"  ========    =======    ==     =    =======    =========    =    =======    =======" << endl;
+	cout <<"  =           =		     = =    =    =              =             =			 =      " << endl;
+	cout <<"  =           ======	 =  =   =    ======         =        =	  =            ===  " << endl;
+	cout <<"  =    ===    =			 =   =  =    =              =        =    =				   =" << endl;
+	cout <<"  =      =    =			 =    = =    =              =        =    =                =" << endl;
+	cout <<"  ========    =======	 =     ==    =======        =        =    =======    =======" << endl;
+
+	cout << "Variation. Every individual in the human population has different characteristics such as eye colour, heightand body mass.Plants of the same species or type can also differ in height, flower colour or leaf length.These differences are called variation." << endl;
+	cout << "The version of a characteristic shown by an individual is called its phenotype." << endl;
+	cout << "Organisms that carry out sexual reproduction are able to combine different forms of genes from two parents, which increases variation in their offspring." << endl;
+	cout <<	"A phenotype is an inherited characteristic.The phenotype of every individual is due to the genes they inherited from their parents."<< endl;
+}
