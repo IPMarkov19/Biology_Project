ba1496100d82b05be323d463169a709585f01796
Teodor Osmanliev <TPOsmanliev19@codingburgas.bg>
2022-05-07 15:03:59 +0300

    Add function for printing cell structure theme

--- BiologyProject/BiologyData/Functions.h
+++ BiologyProject/BiologyData/Functions.h
@@ -18,3 +18,35 @@ void printGeneticsTheme()
 	cout << "Organisms that carry out sexual reproduction are able to combine different forms of genes from two parents, which increases variation in their offspring." << endl;
 	cout <<	"A phenotype is an inherited characteristic.The phenotype of every individual is due to the genes they inherited from their parents."<< endl;
 }
+
+
+void printCellStructureTheme()
+{
+	cout << "=======    =======    =          =      " << endl;
+	cout << "=          =          =          =      " << endl;
+	cout << "=          ======     =          =      " << endl;
+	cout << "=          =          =          =      " << endl;
+	cout << "=          =          =          =      " << endl;
+	cout << "=======    =======    =======    =======" << endl;
+
+	cout << "What are cells?" << endl;
+	cout << "Cells are the building blocks of all living organisms." << endl;
+	cout << "Organisms made of a single cell, such as bacteria and the fungus yeast, are described as being unicellular." << endl;
+	cout << "Organisms made of many cells such as animals, most plants and many species of fungi, are described as being multicellular." << endl;
+	cout << "Multicellular organisms can be made up of millions of cells." << endl;
+	cout << "Cells are microscopic, so to see their structure we need to use microscopes. We can see more structures clearly if we use stains to colour specimens before putting them under the microscope." << endl;
+	cout << "Stains are coloured dyes that are absorbed by some cell structures but not by others. An example of a stain which is used is iodine." << endl;
+
+	cout << endl;
+
+	cout << "The ultrastructure of cells " << endl;
+	cout << "The ultrastructure of a cell is its fine structure as revealed at high magnification. Animal, fungal and plant cells all contain structures called organelles. These are specialised for particular functions. " << endl;
+	cout << "The diagrams below show the similarities and differences between the ultrastructure of animal cells and plant cells. " << endl;
+
+	cout << endl;
+
+	cout << "Structure of fungal and bacterial cells" << endl;
+	cout << "A yeast is a unicellular fungus. Bacterial cells have a more simple structure compared to animal, plant and fungal cells and are usually much smaller." << endl;
+	cout << "They still have a cell membrane and ribosomes, but they lack organelles such as the nucleus. " << endl;
+	cout << "However, bacteria still have DNA, including extra circular pieces of DNA called plasmids.Their cell wall is made of a diffferent material and has a different structure to those of plant and fungal cells." << endl;
+}
--- BiologyProject/BiologyProject/BiologyProject.cpp
+++ BiologyProject/BiologyProject/BiologyProject.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "Menu.h"
+#include "../BiologyData/Functions.h"
 #include "../BiologyData/Data.h"
 
 using namespace std;
