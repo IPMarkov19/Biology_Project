2fd1d06a84b52be00e08a19b178929035836295a
Georgi Milev <GNMilev19@codingburgas.bg>
2022-05-09 01:56:29 +0300

    Fix files

--- BiologyProject/BiologyProject/Menu.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void mainMenu();
-void info();
-void title();
-void selectedOption1(int position, int nextPosition);
-void menu();
--- BiologyProject/BiologyProject/RegisterAndLoginForm.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void login();
-void registration();
-void forgotPassword();
-void selectedOption2(int position, int nextPosition);
-void loginMenu();
